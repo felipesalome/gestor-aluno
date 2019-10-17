ce67a96f160c2dc9c2f2fcf43f0ecd7e4689bbe1
Felipe Salomé <felipesalome_ti@outlook.com>
2019-10-17 14:19:32 -0300

    feat: add Connection DAO

---
+++ gestor-aluno/src/main/java/com/felipe/gestor/dao/Conexao.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.felipe.gestor.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author felipe
+ */
+public class Conexao {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/gestor";
+    private static final String USUARIO = "gestor-aluno";
+    private static final String SENHA = "1234";
+    
+    public static Connection open() {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    
+    public static void close(Connection conn, PreparedStatement pstm, ResultSet rs) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+            if (pstm != null) {
+                pstm.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
