96403f555a7f96aa44ecbb60cb192b6a0a420291
Felipe Salomé <felipesalome_ti@outlook.com>
2019-10-23 19:20:07 -0300

    feat: regex added to course list view

--- gestor-aluno/src/main/java/com/felipe/gestor/controller/helper/AlunoHelper.java
+++ gestor-aluno/src/main/java/com/felipe/gestor/controller/helper/AlunoHelper.java
@@ -8,6 +8,7 @@ package com.felipe.gestor.controller.helper;
 import com.felipe.gestor.model.Aluno;
 import com.felipe.gestor.model.Curso;
 import com.felipe.gestor.view.TelaAluno;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -35,7 +36,7 @@ public class AlunoHelper {
             tableModel.addRow(new Object[]{
                 aluno.getCodigo(),
                 aluno.getNome(),
-                aluno.getCurso()
+                criarLista(aluno.getCurso())
             });
         });
     }
@@ -51,7 +52,9 @@ public class AlunoHelper {
         // adicionando um novo curso
         if (! curso.isEmpty()) {
             Curso setCurso = new Curso(curso);
-            aluno.setCurso(setCurso);
+            List<Curso> cursoList = new ArrayList();
+            cursoList.add(setCurso);
+            aluno.setCurso(cursoList);
         }
         
         return aluno;
@@ -59,7 +62,7 @@ public class AlunoHelper {
     
     public void setarModelo(Aluno modelo) {
         String nome = modelo.getNome();
-        String curso = modelo.getCurso().toString();
+        String curso = limparSentenca(modelo.getCurso().toString());
         
         view.getjTextFieldAlunoNome().setText(nome);
         view.getjTextFieldCursoDescricao().setText(curso);
@@ -72,13 +75,32 @@ public class AlunoHelper {
     
     public String limparSentenca(String inicial) {
         String resultadoFinal = "";
+        
+        // Regex para remover caracteres não alfanuméricos
         String regex = "([\\w])+";
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(inicial);
+        
         while (match.find()) {
             System.out.println(match.group());
             resultadoFinal = match.group();
         }
+        
+        // Retorna a sentena limpa
         return resultadoFinal;
+    }
+
+    public String criarLista(List<Curso> cursosAluno) {
+        String cursos = "";
+        
+        for (Curso curso : cursosAluno) {
+            if (cursos.trim().isEmpty()) {
+                cursos = limparSentenca(curso.toString());
+            } else {
+                cursos = cursos + ", " + limparSentenca(curso.toString());
+            }
+        }
+        
+        return cursos;
     }
 }
--- gestor-aluno/src/test/java/com/felipe/gestor/controller/helper/AlunoHelperTest.java
+++ gestor-aluno/src/test/java/com/felipe/gestor/controller/helper/AlunoHelperTest.java
@@ -5,7 +5,11 @@
  */
 package com.felipe.gestor.controller.helper;
 
+import com.felipe.gestor.dao.AlunoDAO;
+import com.felipe.gestor.model.Aluno;
+import com.felipe.gestor.model.Curso;
 import com.felipe.gestor.view.TelaAluno;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -51,6 +55,60 @@ public class AlunoHelperTest {
         AlunoHelper instance = new AlunoHelper(telaAluno);
         String expResult = "curso";
         String result = instance.limparSentenca(inicial);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void itShouldBeAbleToCreateASentenceOfAListOfCourses() {
+        System.out.println("criarLista");
+        
+        Aluno aluno = new Aluno();
+        
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+        Curso curso3 = new Curso();
+
+
+        List<Curso> cursoList = new ArrayList();
+        
+        AlunoHelper instance = new AlunoHelper(telaAluno);
+        
+        aluno.setCodigo(1);
+        aluno.setNome("Jose");
+        
+        curso1.setCodigo(1);
+        curso1.setDescricao("PHP");
+        curso1.setEmenta("Alguma coisa");
+        
+        curso1.setAluno(aluno);
+        
+        curso2.setCodigo(2);
+        curso2.setDescricao("JAVA");
+        curso2.setEmenta("Alguma coisa");
+        
+        curso2.setAluno(aluno);
+        
+        curso3.setCodigo(3);
+        curso3.setDescricao("HTML");
+        curso3.setEmenta("Alguma coisa");
+        
+        curso3.setAluno(aluno);
+        
+        cursoList.add(curso1);
+        cursoList.add(curso2);
+        cursoList.add(curso3);
+        
+        aluno.setCurso(cursoList);
+        
+        List<Curso> cursos = aluno.getCurso();
+        
+//        cursos.forEach((curso) -> {
+//            System.out.println(curso);
+//        });
+        
+        String expResult = "PHP, JAVA, HTML";
+        String result = instance.criarLista(cursos);
+        
         assertEquals(expResult, result);
     }
     
